2151aa8240b9208ec2998dc149aa74ce5936e8cd
omkartikone <omkartikone379@gmail.com>
2024-05-01 12:36:02 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/twitter',(req, res) =>{
+    res.send('omkartikone')
+})
+app.get('/login',(req, res)=>{
+    res.send("<h1> Please login chai aur code </h1>")
+})
+
+app.get('/Youtube',(req,res)=> {
+    res.send("<h2>chai aur code</h2>")
+})
+
+
+app.listen(process.env.port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
